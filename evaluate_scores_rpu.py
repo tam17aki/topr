3968149fbbbf3b8b53def82ec4cf21beb1a45c09
Akira Tamamori <tamamori5917@gmail.com>
2024-06-19 21:03:12 +0900

    fix to use cuda()

--- evaluate_scores_rpu.py
+++ evaluate_scores_rpu.py
@@ -42,24 +42,23 @@ from torch.multiprocessing import set_start_method
 from model import get_model
 
 
-def load_checkpoint(cfg: DictConfig, device):
+def load_checkpoint(cfg: DictConfig):
     """Load checkpoint.
 
     Args:
         cfg (DictConfig): configuration.
-        device: device info.
 
     Returns:
         model_bpd (nn.Module): DNNs to estimate BPD.
         model_fpd (nn.Module): DNNs to estimate FPD.
     """
     model_dir = os.path.join(cfg.TOPR.root_dir, cfg.TOPR.model_dir)
-    model_bpd = get_model(cfg, device)
+    model_bpd = get_model(cfg)
     model_file = os.path.join(model_dir, cfg.training.model_file + ".bpd.pth")
     checkpoint = torch.load(model_file)
     model_bpd.load_state_dict(checkpoint)
 
-    model_fpd = get_model(cfg, device)
+    model_fpd = get_model(cfg)
     model_file = os.path.join(model_dir, cfg.training.model_file + ".fpd.pth")
     checkpoint = torch.load(model_file)
     model_fpd.load_state_dict(checkpoint)
@@ -359,7 +358,6 @@ def _reconst_waveform(cfg, model_tuple, logmag_path):
     Returns:
         None.
     """
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     logmag = np.load(logmag_path)  # [T, K]
     magnitude = np.exp(logmag)  # [T, K]
 
@@ -367,7 +365,7 @@ def _reconst_waveform(cfg, model_tuple, logmag_path):
     logmag = np.pad(
         logmag, ((cfg.model.n_lookback, cfg.model.n_lookahead), (0, 0)), "constant"
     )
-    logmag = torch.from_numpy(logmag).float().unsqueeze(0).to(device)  # [1, T+L+1, K]
+    logmag = torch.from_numpy(logmag).float().unsqueeze(0).cuda()  # [1, T+L+1, K]
     tpd, fpd = compute_1st_stage(cfg, model_tuple, logmag)
 
     # reconstruct phase spectra by using instantaneous frequency (= TPD) and
@@ -481,8 +479,9 @@ def main(cfg: DictConfig):
     os.makedirs(score_dir, exist_ok=True)
 
     # load DNN parameters
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model_bpd, model_fpd = load_checkpoint(cfg, device)
+    model_bpd, model_fpd = load_checkpoint(cfg)
+    model_bpd.cuda()
+    model_fpd.cuda()
     model_bpd.eval()
     model_fpd.eval()
 
